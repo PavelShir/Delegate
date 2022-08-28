52b4469417717e56068a7964985b382620dbb787
Pavel Shirokiy <pshirokiy@gmail.com>
2022-08-28 22:01:34 +0300

    done

--- Delegate/FirstViewController.swift
+++ Delegate/FirstViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate {
+    
+    func tranferSmile(labelSmile: String)
+}
+
 class FirstViewController: UIViewController {
     
     @IBOutlet var smileLabel: UILabel!
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let secondVC = segue.destination as? SecondViewController else { return }
+        secondVC.delegate = self
+        secondVC.smileLabelText = smileLabel.text
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+}
+
+extension FirstViewController: SecondViewControllerDelegate {
+    
+    func tranferSmile(labelSmile: String) {
+        smileLabel.text = labelSmile
     }
 }
 
--- Delegate/SecondViewController.swift
+++ Delegate/SecondViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var textField: UITextField!
     @IBOutlet var smileLabel: UILabel!
     
+    var smileLabelText: String!
+    var delegate: SecondViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        smileLabel.text = smileLabelText
+        textField.delegate = self
     }
 
     @IBAction func goBackButtonPressed() {
-
+        smileLabelText = smileLabel.text
+        delegate.tranferSmile(labelSmile: smileLabelText)
+        dismiss(animated: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         textField.endEditing(true)
+        textFieldDidEndEditing(textField)
     }
+    
 }
 
+extension SecondViewController {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let textValue = textField.text else { return }
+       
+        switch textValue {
+        case "😀", "😎", "🙃", "🤩", "🤬", "🥸", "🙂":
+            smileLabel.text = textValue
+        default:
+            showAlert(with: "Please, enter correct info", and: "Only this: 😀, 😎, 🙃, 🤩, 🤬, 🥸, 🙂")
+                return
+            }
+        }
+    }
+
+extension SecondViewController {
+  
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+}
